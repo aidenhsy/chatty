d3022c068d5797e3f9acb9e88b42d9e636e178b2
Aiden Yang <aidenyang@Aidens-MacBook-Air.local>
2021-01-13 14:10:52 +0800

    updates

--- frontend/src/components/Messages/Message/Message.js
+++ frontend/src/components/Messages/Message/Message.js
@@ -5,7 +5,7 @@ import './Message.css';
 import ReactEmoji from 'react-emoji';
 import { UserContext } from '../../../contexts/UserContext';
 
-const Message = ({ message: { message, sender, receiver } }) => {
+const Message = ({ message: { message, sender } }) => {
   const { user } = useContext(UserContext);
   let isSentByCurrentUser = false;
 
@@ -15,7 +15,6 @@ const Message = ({ message: { message, sender, receiver } }) => {
 
   return isSentByCurrentUser ? (
     <div className="messageContainer justifyEnd">
-      <p className="sentText pr-10">{sender.username}</p>
       <div className="messageBox backgroundBlue">
         <p className="messageText colorWhite">{ReactEmoji.emojify(message)}</p>
       </div>
@@ -25,7 +24,6 @@ const Message = ({ message: { message, sender, receiver } }) => {
       <div className="messageBox backgroundLight">
         <p className="messageText colorDark">{ReactEmoji.emojify(message)}</p>
       </div>
-      <p className="sentText pl-10 ">{receiver.username}</p>
     </div>
   );
 };
